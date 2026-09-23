d19bb5671652ad638bf4ad77b5027128d3326cb5
Emka <dumbliauskasemilis@gmail.com>
2026-09-23 22:13:18 +0300

    IDk

--- src/main/java/org/example/Exercise1.java
+++ src/main/java/org/example/Exercise1.java
@@ -1,24 +1,32 @@
 package org.example;
 
-import java.util.LinkedList;
 
 public class Exercise1 {
-    private LinkedList<Owner> contacts = new LinkedList<>();
-    public Owner binSearch(String name, int low, int high){
-            while (low <= high) {
-                int mid = low + (high - low) / 2;
+    public static void main(String[] args) {
+        Owner LinkedList = new Owner();
+
+        Owner contacts = new Owner();
+
+        contacts.addNode("Bob");
+        contacts.addNode("Beatrice");
+        contacts.addNode("Emka");
+        contacts.addNode("Omar");
+
+        System.out.println(binSearch("Emka", 0, 3));
+    }
+
+    static Owner contacts = new Owner();
+    public static Owner binSearch(String name, int low, int high){
+                int mid = (low + high) / 2;
 
                 if (contacts.get(mid).toString().equalsIgnoreCase(name))
                     return contacts.get(mid);
 
                 if (contacts.get(mid).toString().compareToIgnoreCase(name) < 0)
-                    low = mid + 1;
+                    return binSearch(name, mid + 1, high);
 
                 else
-                    high = mid - 1;
-            }
-
-        return null;
+                    return binSearch(name, low, mid - 1);
     }
 }
 
--- src/main/java/org/example/Owner.java
+++ src/main/java/org/example/Owner.java
@@ -1,5 +1,52 @@
 package org.example;
 
 public class Owner {
+    private String data;
+    private Owner pointer;
+
+    public Owner(){}
+    public Owner(String d){
+        this.data = d;
+        this.pointer = null;
+    }
+
+    private Owner head;
+    private Owner tail;
+
+    private static int counter;
+
+    public void addNode(String data){
+        Owner newNode = new Owner(data);
+        
+        if(this.head == null){
+            counter = 1;
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.pointer = newNode; // HERE we reference the OLD TAIL and give a pointer
+            // to the new/upcoming tail
+
+            this.tail = newNode;
+            counter++;
+        }
+    }
+
+    public Owner get(int index){
+        Owner current = head;
+        int currentIndex = 0;
+
+        while (current != null || currentIndex < index){
+            current = current.pointer;
+            currentIndex++;
+        }
+
+        return current;
+    }
+
+    @Override
+    public String toString(){
+        return data;
+    }
+
 
 }
